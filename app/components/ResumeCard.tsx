0a7d4938afe05cb266744930aa084459e149d379
Sreshtha Mehrotra <sreshtha.mehrotra@gmail.com>
2025-08-16 01:11:47 +0530

    impl resume card

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router";
+
+const ResumeCard = ({resume}: {resume:Resume }) => {
+    return (
+        <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000" >
+
+        </Link>
+    )
+}
+
+export default ResumeCard;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "Welcome to Resume Analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+      return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+          <Navbar></Navbar>
+          <section className="main-section">
+              <div className="page-heading">
+                  <h1>Track Your Resume Ratings</h1>
+                  <h2>Review Your AI-powered Feedback</h2>
+              </div>
+          </section>
+
+          {resumes.length > 0 && (
+              <div className="resume-section">
+                  {
+                      resumes.map((resume) => (
+                          <div>
+                              <ResumeCard key={resume.id} resume={resume}/>
+                          </div>
+                      ))
+                  }
+              </div>
+          ) }
+
+      </main>;
 }
