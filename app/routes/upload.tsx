7787ac4453f4e57c4ae148096d9ce28c5b056c36
Sreshtha Mehrotra <sreshtha.mehrotra@gmail.com>
2025-08-17 19:24:41 +0530

    added uploader route

---
+++ app/routes/upload.tsx
@@ -0,0 +1,56 @@
+import React, {type FormEvent} from "react";
+import Navbar from "~/components/Navbar";
+
+const Upload = () => {
+
+    const [isProcessing, setIsProcessing] = React.useState(false);
+    const [statusText, setStatusText] = React.useState("");
+
+
+    const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
+
+    }
+
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar></Navbar>
+            <section className="main-section">
+                <div className="page-heading py-16">
+                    <h1>Smart Feedback for your dream job</h1>
+                    {
+                        isProcessing ? (<>
+                            <h2>{statusText}</h2>
+                            <img src="/images/resume-scan-2.gif" className="w-full"/>
+                        </>): (<h2>Drop your Resume for an ATS score and feedback</h2>)
+                    }
+
+                    {!isProcessing && (
+                        <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
+                            <div className="form-div">
+                                <label htmlFor="company-name">Company Name</label>
+                                <input type="text" name="company-name" placeholder="Company Name" id="company-name"/>
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="company-name">Company Name</label>
+                                <input type="text" name="company-name" placeholder="Company Name" id="company-name"/>
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="job-description">Job Description</label>
+                                <textarea rows={5} name="job-description" placeholder="Job Description" id="job-description"/>
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="uploader">Upload Resume</label>
+                                <div>Uploader</div>
+                            </div>
+
+                            <button type="submit" className="primary-button">Analyze Resume</button>
+                        </form>
+                    )}
+                </div>
+            </section>
+        </main>
+    )
+}
+
+
+export default Upload;
